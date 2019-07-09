2d99686f6616851a3ed9232cf0dff791ba9220da
Daria Lykova <daria.lykova@gmail.com>
2019-07-09 16:53:40 +0200

    Writes basic and assertion steps code for testing

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -1,3 +1,7 @@
 Then("I should see {string}") do |content|
   expect(page).to have_content content
 end
+
+Then("I should be on the {string}") do |menu_page|
+  visit menu_page_path
+end
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,3 +1,8 @@
 When("I visit the site") do
   visit root_path
 end
+
+
+Given("I click {string}") do |link|
+  click_on link
+end

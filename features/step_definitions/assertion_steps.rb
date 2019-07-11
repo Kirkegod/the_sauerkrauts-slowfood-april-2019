8eca1ba1f5edeac6e1b105c960cd755c637d0cfd
Daria Lykova <daria.lykova@gmail.com>
2019-07-11 17:22:05 +0200

    Trying to connect the haml file to the test file to show category id/Tests not passing/Haml syntax has to be fixed

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -4,9 +4,9 @@ end
 
 Then("I should see {string} in the {string} category") do |product, category|
   product_category = Category.find_by(name: category)
-  # dom_section = "#category_#{product_category.id}"
-  # within(dom_section) do
-  #   expect(page).to have_content product
-  # end
+  dom_section = "#category_#{product_category.id}"
+  within(dom_section) do
+    expect(page).to have_content product
+  end
 end
 

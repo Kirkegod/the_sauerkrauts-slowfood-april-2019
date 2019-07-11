d703fdac700ddc1ebb9260efea24845290686fa7
erka <erik.westerberg@hotmail.se>
2019-07-11 13:28:58 +0200

    create the initializer, routes, and modify your assets for Cartify

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :products, only: [:show]
+  mount Cartify::Engine, at: '/'
   devise_for :users, controllers: { registrations: "registrations" }
   root controller: :products, action: :index
 end

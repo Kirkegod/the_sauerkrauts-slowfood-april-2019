d3b4fc4df160b10efd16e2305faa591177edbd7d
Daria Lykova <daria.lykova@gmail.com>
2019-07-11 10:25:49 +0200

    Changing the name of the spec file

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -2,7 +2,7 @@ Then("I should see {string}") do |content|
   expect(page).to have_content content
 end
 
-Then("I should see {string} in the {string} category") do |string, string2|
+Then("I should see {string} in the {string} category") do |product, category|
   pending # Write code here that turns the phrase above into concrete actions
 end
 

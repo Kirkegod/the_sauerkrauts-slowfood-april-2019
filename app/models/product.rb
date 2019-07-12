8a4a7026f10050ddd032cb6801ffc085e9eaef37
Kirkegaard <milla.kirkegaard@gmail.com>
2019-07-12 11:47:38 +0200

    fixed parentheses and indentation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,3 +2,4 @@ class Product < ApplicationRecord
     belongs_to :category
     validates_presence_of :name, :description, :price
 end
+
--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -4,6 +4,7 @@ end
 
 Then("I should see {string} in the {string} category") do |product, category|
   product_category = Category.find_by(name: category)
+  binding.pry
   dom_section = "#category_#{product_category.id}"
   within(dom_section) do
     expect(page).to have_content product
